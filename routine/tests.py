5faa1fa1e8e53b7ac23e6cc12cab363856992c73
limcpf <matathresh1740@naver.com>
2023-02-04 22:40:53 +0900

    feat: 일부 Routine 임시 테스트 추가

--- routine/tests.py
+++ routine/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
 
-# Create your tests here.
+from routine.enums import Days, RoutineCategory, DaysChoices
+from routine.models import RoutineDay, Routine
+from user.tests import path_create, path_login
+
+
+class EnumTests(TestCase):
+    def setUp(self):
+        account = self.client.post(
+            path=path_create,
+            data={
+                "email": "limc@test.co.kr",
+                "password1": "test1234!",
+                "password2": "test1234!"
+            }
+        )
+
+        self.account_id = account.json()["user"]["pk"]
+
+        routine = Routine.objects.create(
+            title="title",
+            category="H",
+            goal="goal",
+            is_alarm=False,
+            account_id=self.account_id
+        )
+
+        self.routine_id = routine.routine_id
+
+    def test_enum_value(self):
+        for day in ["MON", "TUE"]:
+            day = Days[day].value
+            RoutineDay.objects.create(day=day, routine_id=self.routine_id)
+
