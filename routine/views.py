311a3e4b61d9f3b1ae62a696bc37ae344d862843
limcpf <matathresh1740@naver.com>
2023-02-04 22:41:14 +0900

    feat: Routine list, create view 추가

--- routine/views.py
+++ routine/views.py
@@ -1,3 +1,46 @@
+from django.db.models import OuterRef
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from routine.models import Routine, RoutineDay, RoutineResult
+from routine.serializers import RoutineSerializer
+
+
+class RoutineViewSet(viewsets.ModelViewSet):
+    routine_id = None
+    queryset = Routine.objects.all()
+    serializer_class = RoutineSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def list(self, request, *args, **kwargs):
+        queryset = Routine.objects\
+            .live(self.request.user.id)\
+            .filter(
+                routineday__day=0
+            )\
+            .annotate(
+                result=RoutineResult.objects.filter(
+                    routine_id=OuterRef("routine_id")).values("result")
+            )
+        serializer = RoutineSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        super().create(request, *args, **kwargs)
+        return Response(
+            {
+                "data": {
+                    "routine_id": self.routine_id
+                },
+                "message": {
+                    "msg": "You have successfully created the routine.",
+                    "status": "ROUTINE_CREATE_OK"
+                }
+            },
+            status=201
+        )
+
+    def perform_create(self, serializer):
+        routine = serializer.save(account_id=self.request.user.id)
+        self.routine_id = routine.routine_id
