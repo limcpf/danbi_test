c1c9d4f6eb1aa957b16cd888fc65216999020dc5
limcpf <matathresh1740@naver.com>
2023-02-05 12:41:38 +0900

    fix: 요일 관련 로직 추가

--- routine/views.py
+++ routine/views.py
@@ -1,7 +1,9 @@
+from datetime import date, datetime
+
 from django.db.models import OuterRef
-from django.shortcuts import render
+from django.utils.datastructures import MultiValueDictKeyError
 from rest_framework.response import Response
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, exceptions, status
 
 from routine.models import Routine, RoutineDay, RoutineResult
 from routine.serializers import RoutineSerializer
@@ -14,15 +16,28 @@ class RoutineViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     def list(self, request, *args, **kwargs):
-        queryset = Routine.objects\
-            .live(self.request.user.id)\
+        day = None
+        today = None
+
+        try:
+            today = datetime.strptime(self.request.query_params['today'], "%Y-%m-%d")
+            day = date.weekday(today)
+        except MultiValueDictKeyError:
+            today = datetime.today()
+            day = today.weekday()
+        except ValueError:
+            raise exceptions.APIException(detail="올바른 날짜 형식(yyyy-mm-dd)이 아닙니다.", code=status.HTTP_400_BAD_REQUEST)
+
+        queryset = Routine.objects \
+            .live(self.request.user.id) \
             .filter(
-                routineday__day=0
-            )\
+            routineday__day=day
+        ) \
             .annotate(
-                result=RoutineResult.objects.filter(
-                    routine_id=OuterRef("routine_id")).values("result")
-            )
+            result=RoutineResult.objects.filter(
+                routine_id=OuterRef("routine_id")).values("result")
+        )
+
         serializer = RoutineSerializer(queryset, many=True)
         return Response(serializer.data)
 
