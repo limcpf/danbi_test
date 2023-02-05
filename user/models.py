a7bccb33959afc531c1e74376574fe4964f3da14
limcpf <matathresh1740@naver.com>
2023-02-05 23:26:23 +0900

    fix: uuid 충돌 에러로 model 수정

--- user/models.py
+++ user/models.py
@@ -6,26 +6,13 @@ from django.db import models
 from user.managers import CustomUserManager
 
 
-def get_hex_uuid():
-    """
-    uuid 생성 시 dash(-)를 제외한 문자열을 return 해주는 함수
-    :return: 무작위 32자리 숫자+영어 문자열
-    """
-    return uuid.uuid4().hex
-
-
 class User(AbstractUser):
     """
     id - UUID에서 dash(-)를 제외한 32자리 값
     username - 별도로 사용하지 않음
     email - 이메일을 아이디로 사용하는 user
     """
-    id = models.CharField(
-        default=get_hex_uuid(),
-        primary_key=True,
-        editable=False,
-        max_length=32
-    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     username = None
     email = models.EmailField(
@@ -39,4 +26,4 @@ class User(AbstractUser):
     objects = CustomUserManager()
 
     def __str__(self):
-        return self.id
+        return self.id.__str__()
