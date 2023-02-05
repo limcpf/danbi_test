10d2e9963bb79a3af955a0ce60bbf6c60b626925
limcpf <matathresh1740@naver.com>
2023-02-05 17:09:33 +0900

    fix: modelSerializer 로 수정

--- routine/serializers.py
+++ routine/serializers.py
@@ -1,43 +1,60 @@
 from django.db import transaction
 from rest_framework import serializers, exceptions, status
+from rest_framework.serializers import ModelSerializer
 
 from routine.enums import RoutineCategoryChoices, DaysChoices, Days, RoutineResultChoices
 from routine.models import Routine, RoutineResult, RoutineDay
 
 
-class RoutineSerializer(serializers.Serializer):
+class RoutineDaySerializer(ModelSerializer):
+    class Meta:
+        model = RoutineDay
+        fields = ["day"]
+
+
+class RoutineResultSerializer(ModelSerializer):
+    class Meta:
+        model = RoutineResult
+        fields = ["result"]
+
+
+class RoutineSerializer(ModelSerializer):
+    days = serializers.SerializerMethodField()
+    result = serializers.SerializerMethodField()
+
     class Meta:
         model = Routine
-        fields = ["routine_id", "title", "category", "goal", "days"]
+        fields = ["routine_id", "title", "category", "goal", "is_alarm", "days", "result"]
+        read_only_fields = ["result"]
 
-    routine_id = serializers.CharField(label="id", max_length=32, read_only=True)
-    title = serializers.CharField(max_length=255, initial="title")
-    category = serializers.ChoiceField(
-        choices=RoutineCategoryChoices,
-        initial=RoutineCategoryChoices.HOMEWORK.value
-    )
-    goal = serializers.CharField(initial="goal")
+    def get_days(self, instance):
+        return list(map(str, RoutineDay.objects.filter(routine=instance)))
+
+    def get_result(self, instance):
+        return RoutineResult.objects.get(routine=instance).result  # "H"
+
+
+class RoutineCreateSerializer(ModelSerializer):
+    class Meta:
+        model = Routine
+        fields = ["routine_id", "title", "category", "goal", "is_alarm", "days"]
+
     days = serializers.ListField(
         child=serializers.ChoiceField(choices=DaysChoices.names),
         required=False,
         initial=["MON"]
     )
-    result = serializers.CharField(read_only=True)
 
     @transaction.atomic()
     def create(self, data):
-        data.pop("days")  # days는 routine 을 만드는데 사용하지 않음
+        days = data.pop("days")  # days는 routine 을 만드는데 사용하지 않음
         routine = Routine.objects.create(**data)
-        self.create_result(routine.routine_id)
+        RoutineResult.objects.create(routine_id=routine.routine_id)
         try:
-            days = self.context["request"].data["days"]
             self.create_day(days, routine.routine_id)
         except KeyError as e:
             raise exceptions.APIException(detail="올바른 day 값이 아닙니다.", code=status.HTTP_400_BAD_REQUEST)
         return routine
-
-    def create_result(self, routine_id):
-        RoutineResult.objects.create(routine_id=routine_id)
 
     def create_day(self, days, routine_id):
         if len(days) < 1:
@@ -46,3 +63,4 @@ class RoutineSerializer(serializers.Serializer):
         for day in days:
             day = Days[day].value
             RoutineDay.objects.create(day=day, routine_id=routine_id)
+#
